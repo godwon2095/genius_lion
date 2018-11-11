dfa79cd6bc179bdd27be85b10a786bed7f002bf1
sungwon <jsw2095@naver.com>
2018-11-11 21:02:01 +0900

    google kakao login

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,30 +1,32 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
+  def self.provides_callback_for(provider)
+    # byebug
+    class_eval %Q{
+      def #{provider}
+        @user = User.find_for_oauth(request.env["omniauth.auth"], current_user)
+        if @user.persisted?
+          sign_in_and_redirect @user, event: :authentication
+        else
+          session["devise.#{provider}_data"] = request.env["omniauth.auth"]
+          redirect_to new_user_registration_url, notice: '로그인 에러가 발생하였습니다.'
+        end
+      end
+    }
+  end
 
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
+  [:google_oauth2, :naver, :kakao].each do |provider|
+    provides_callback_for provider
+  end
 
-  # More info at:
-  # https://github.com/plataformatec/devise#omniauth
-
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
-
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
-
-  # protected
-
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
+  def after_sign_in_path_for(resource)
+    auth = request.env['omniauth.auth']
+    @identity = Identity.find_for_oauth(auth)
+    if current_user.persisted?
+      root_path
+    else
+      new_user_registration_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
 
   devise_for :users, controllers: {
-   sessions: 'users/sessions'
+   sessions: 'users/sessions',
+   omniauth_callbacks: 'users/omniauth_callbacks'
   }
 
   devise_scope :user do
