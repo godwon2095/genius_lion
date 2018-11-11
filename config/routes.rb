ce84bf117472cbc188330b8726fc5ff08134b348
sungwon <jsw2095@naver.com>
2018-11-11 18:26:22 +0900

    User controller setting

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  devise_for :users
+  devise_for :users, controllers: {
+   sessions: 'users/sessions'
+  }
+
+  devise_scope :user do
+    get 'users/before_sign_in' => "users/sessions#before_sign_in", as: 'before_sign_in'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
