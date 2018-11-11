8323c656560c2c6c899110d7847dadf24254ab43
sungwon <jsw2095@naver.com>
2018-11-11 17:13:33 +0900

    devise admin setting

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
