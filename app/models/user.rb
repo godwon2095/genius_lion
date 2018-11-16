4935c7f188140583844026ed9b6838a8a91b8074
donaskwhy <donaskwhy@nate.com>
2018-11-16 20:08:22 +0900

    explaination

--- app/models/user.rb
+++ app/models/user.rb
@@ -49,7 +49,7 @@ class User < ApplicationRecord
     user #user 리턴
   end
 
-  def is_friend(other_user)
+  def is_friend(other_user) #친구인지 확인
     result = false
     tmp_friend1 = Friend.where(user1: self)
     tmp_friend2 = Friend.where(user2: self)
@@ -66,7 +66,7 @@ class User < ApplicationRecord
     return result
   end
 
-  def sent_friend(other_user)
+  def sent_friend(other_user) #A가 B에게 친구요청을 보낸 상태인지 확인
     result = false
     friendrequest = UserAlarm.where(send_user: self)
     if friendrequest.present?
@@ -77,7 +77,7 @@ class User < ApplicationRecord
     return result
   end
 
-  def received_friend(other_user)
+  def received_friend(other_user) #A가 B로부터 친구요청을 받은 상태인지 확인
     result = false
     friendrequest = UserAlarm.find_by(user_id: self.id, send_user_id: other_user.id)
     if friendrequest.present?
