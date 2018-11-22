6f46adfefd1deff15d6259aa040f9e37600e8d53
sungwon <jsw2095@naver.com>
2018-11-22 17:44:09 +0900

    remove rooms game foreign key

---
+++ db/migrate/20181122084321_remove_foreign_key_from_rooms.rb
@@ -0,0 +1,5 @@
+class RemoveForeignKeyFromRooms < ActiveRecord::Migration[5.2]
+  def change
+    remove_reference :rooms, :game, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_21_161538) do
+ActiveRecord::Schema.define(version: 2018_11_22_084321) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
