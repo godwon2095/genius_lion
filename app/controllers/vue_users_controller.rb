ffc555c3c906b27c89a68df0bf2e0dcc5ab15675
sungwon <jsw2095@naver.com>
2018-11-23 04:59:29 +0900

    password test

--- app/controllers/vue_users_controller.rb
+++ app/controllers/vue_users_controller.rb
@@ -6,7 +6,7 @@ class VueUsersController < ApplicationController
     user = User.find_by(email: email)
     if user.nil?
       new_password = "password"
-      user = User.create!(email: email,  password: new_password, password_confirmation: new_password, name: name, remote_image_url: image)
+      user = User.create!(email: email,  password: new_password, password_confirmation: new_password, name: name, image: image)
 
       render json: {result: "create", user: user}
     else
