1ddad0cfc3f8e125f109bb2ea4ba1f7955721a70
sungwon <jsw2095@naver.com>
2018-11-23 05:36:04 +0900

    item

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 # AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
 # User.create!(email: 'jsw2095@naver.com', name: "seongwon", password: "111111", password_confirmation: "111111")
-User.create!(email: 'qwe@naver.com', name: 'donaskwhy', ruby_count: '3', join_game_count: '5', win_game_count: '3', password: 'password', password_confirmation: 'password')
-User.create!(email: 'asd@naver.com', name: 'geniuslion', ruby_count: '1', join_game_count: '5', win_game_count: '1', password: 'password', password_confirmation: 'password')
-User.create!(email: 'zxc@naver.com', name: '퍄퍄', ruby_count: '2', join_game_count: '5', win_game_count: '2', password: 'password', password_confirmation: 'password') 
+# User.create!(email: 'qwe@naver.com', name: 'donaskwhy', ruby_count: '3', join_game_count: '5', win_game_count: '3', password: 'password', password_confirmation: 'password')
+# User.create!(email: 'asd@naver.com', name: 'geniuslion', ruby_count: '1', join_game_count: '5', win_game_count: '1', password: 'password', password_confirmation: 'password')
+# User.create!(email: 'zxc@naver.com', name: '퍄퍄', ruby_count: '2', join_game_count: '5', win_game_count: '2', password: 'password', password_confirmation: 'password')
 # Game.find_or_create_by(title)
-# Item.generate_items
+Item.generate_items
