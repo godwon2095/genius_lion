3ff3b6aab343d09dc3a4917f9b06564baa2c1d19
sungwon <jsw2095@naver.com>
2018-11-23 04:55:51 +0900

    create user vue

--- app/controllers/vue_users_controller.rb
+++ app/controllers/vue_users_controller.rb
@@ -1,10 +1,12 @@
 class VueUsersController < ApplicationController
   def create
     email = params[:email]
+    name = params[:name]
+    image = params[:image]
     user = User.find_by(email: email)
     if user.nil?
       new_password = Devise.friendly_token[0,20]
-      user = User.create!(email: email, password: new_password, password_confirmation: new_password)
+      user = User.create!(email: email,  password: new_password, name: name, remote_image_url: image, password_confirmation: new_password)
 
       render json: {result: "create", user: user}
     else
